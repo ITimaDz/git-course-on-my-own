992f556023b62662c24e1c7d00ba43db9d63ebaa
ITimaDz <dzhuraev-t@mail.ru>
2020-11-21 13:46:34 +0500

    Second one

--- main.py
+++ main.py
@@ -1,20 +1,61 @@
-def checkio(data):
-    base = "I" * data
+rule_add = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+}
 
-    base = base.replace("I" * 5, "V")
-    base = base.replace("V" * 2, "X")
-    base = base.replace("X" * 5, "L")
-    base = base.replace("L" * 2, "C")
-    base = base.replace("C" * 5, "D")
-    base = base.replace("D" * 2, "M")
+rule_div = {
+    ('I', 'V'): 3,
+    ('I', 'X'): 8,
+    ('X', 'L'): 30,
+    ('X', 'C'): 80,
+    ('C', 'D'): 300,
+    ('C', 'M'): 800,
+}
 
-    base = base.replace("DCCCC", "CM")
-    base = base.replace("CCCC", "CD")
-    base = base.replace("LXXXX", "XC")
-    base = base.replace("XXXX", "XL")
-    base = base.replace("VIIII", "IX")
-    base = base.replace("IIII", "IV")
+def roman_to_arabic(roman_number):
+    number = 0
+    prev_literal = None
+    for literal in roman_number:
+        if prev_literal and rule_add[prev_literal] < rule_add[literal]:
+            number += rule_div[(prev_literal, literal)]
+        else:
+            number += rule_add[literal]
+        prev_literal = literal
+    return number
 
-    return base
+import unittest
 
-print(checkio(25))
+class RomanNumTest(unittest.TestCase):
+    def test_roman_num(self):
+        self.assertEquals(roman_to_arabic('I'), 1)
+        self.assertEquals(roman_to_arabic('II'), 2)
+        self.assertEquals(roman_to_arabic('III'), 3)
+        self.assertEquals(roman_to_arabic('IV'), 4)
+        self.assertEquals(roman_to_arabic('V'), 5)
+        self.assertEquals(roman_to_arabic('VI'), 6)
+        self.assertEquals(roman_to_arabic('VII'), 7)
+        self.assertEquals(roman_to_arabic('VIII'), 8)
+        self.assertEquals(roman_to_arabic('IX'), 9)
+        self.assertEquals(roman_to_arabic('X'), 10)
+        self.assertEquals(roman_to_arabic('XXXI'), 31)
+        self.assertEquals(roman_to_arabic('XLVI'), 46)
+        self.assertEquals(roman_to_arabic('XCIX'), 99)
+        self.assertEquals(roman_to_arabic('DLXXXIII'), 583)
+        self.assertEquals(roman_to_arabic('DCCCLXXXVIII'), 888)
+        self.assertEquals(roman_to_arabic('MDCLXVIII'), 1668)
+        self.assertEquals(roman_to_arabic('MCMLXXXIX'), 1989)
+        self.assertEquals(roman_to_arabic('MMX'), 2010)
+        self.assertEquals(roman_to_arabic('MMXI'), 2011)
+        self.assertEquals(roman_to_arabic('MMXII'), 2012)
+        self.assertEquals(roman_to_arabic('MMMCMXCIX'), 3999)
+
+print(roman_to_arabic('III'))
+print(roman_to_arabic('IV'))
+print(roman_to_arabic('IX'))
+print(roman_to_arabic('LVIII'))
+print(roman_to_arabic('MCMXCIV'))
